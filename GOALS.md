0765a36985e538968aba1e34181fa9d7d5497c81
nigelhaney <nigel.haney27@gmail.com>
2024-03-22 16:44:57 -0700

    Create nix flake dev shell for project.

---
+++ GOALS.md
@@ -0,0 +1,5 @@
+# Goals for my website
+
+* [ ]
+* [ ] Create nix flake for development shell that can build the site
+* [ ] Create github action that builds the site using nix and deploys the site to cloudflare pages
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Flake containing build environment for nigelhaney.com";
+
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            nodejs
+            python3
+          ];
+        };
+      }
+    );
+}
