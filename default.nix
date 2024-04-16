b2daed835528de12df025c810ef0c5ba2816f470
nigelhaney <nigel.haney27@gmail.com>
2024-04-15 21:58:35 -0700

    Try to configure fonts with fontconfig nix. Partially working but not selecting the right fonts.

--- default.nix
+++ default.nix
@@ -1,19 +1,39 @@
-{ stdenv, tailwindcss }:
+{ stdenv, python3, chromium, makeFontsConf, noto-fonts-emoji, fira-code, dejavu_fonts }:
+let 
+    fontPkgsForChrome = [
+        dejavu_fonts
+        noto-fonts-emoji
+        fira-code
+    ];
+in
 stdenv.mkDerivation {
     pname = "nigelhaney.com";
     version = "1.0.0";
-    src = ./.;
+    src = ./src;
 
     nativeBuildInputs = [
-        tailwindcss
-    ];
+        python3
+        chromium
+    ] ++ fontPkgsForChrome;
+
+    # Chrome won't load any fonts by default, add a font config file so it does.
+    FONTCONFIG_FILE = makeFontsConf {
+        fontDirectories = fontPkgsForChrome;
+    };
 
     buildPhase = ''
-        tailwindcss -i css/styles.css -c css/tailwind.config.js --output public/css/tailwind.css
-    '';
-
-    installPhase = ''
+        # Prepare output directory
         mkdir -p $out
-        cp -rf ./public $out
+
+        # Copy html files from src to output directory
+        cp -rf . $out/
+
+        cp $FONTCONFIG_FILE $out/
+
+        # Use chrome to create PDF of resume.
+        python -m http.server -d $out &
+        serverpid=$!
+        chromium-browser --no-sandbox --headless --no-margins --no-pdf-header-footer --print-to-pdf-no-header --run-all-compositor-stages-before-draw --print-to-pdf=$out/resources/resume.pdf $out/resume/index.html
+        kill $serverpid
     '';
 }
