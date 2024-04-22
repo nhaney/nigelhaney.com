14ff610831103cb3272aa2537c1d737b914da4f9
nigelhaney <nigel.haney27@gmail.com>
2024-04-22 00:00:36 -0700

    trying to make resume my responsive.

--- default.nix
+++ default.nix
@@ -28,18 +28,6 @@ stdenv.mkDerivation {
         # Copy html files from src to output directory
         cp -rf . $out/
 
-        # Copy fonts to resources in order to self-host fonts.
-        mkdir -p $out/resources/fonts
-
-        #cp ${fira-code}/share/fonts/truetype/FiraCode-VF.ttf $out/resources/fonts/fira-code.ttf
-
-        #cp ${dejavu_fonts}/share/fonts/truetype/DejaVuSans.ttf $out/resources/fonts/dejavu-sans.ttf
-        #cp ${dejavu_fonts}/share/fonts/truetype/DejaVuSans-Bold.ttf $out/resources/fonts/dejavu-sans-bold.ttf
-        #cp ${dejavu_fonts}/share/fonts/truetype/DejaVuSans-Oblique.ttf $out/resources/fonts/dejavu-sans-italic.ttf
-
-        ## TODO: See if we can reduce the size of this by only including emojis we need.
-        #cp ${noto-fonts-emoji}/share/fonts/noto/NotoColorEmoji.ttf $out/resources/fonts/noto-color-emoji.ttf
-
         # Use chrome to create PDF of resume.
         chromium-browser --no-sandbox --headless --no-margins --no-pdf-header-footer --print-to-pdf-no-header --run-all-compositor-stages-before-draw --print-to-pdf=$out/resources/resume.pdf $out/resume/index.html
     '';
