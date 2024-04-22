0ac74f8d0e710e3447ce1d9df71b95aa0e1004a2
nigelhaney <nigel.haney27@gmail.com>
2024-04-21 23:35:17 -0700

    Got resume building with local css font query.

--- default.nix
+++ default.nix
@@ -1,24 +1,24 @@
 { stdenv, python3, chromium, makeFontsConf, noto-fonts-emoji, fira-code, dejavu_fonts }:
 let 
-    fontPkgsForChrome = [
+    fontPkgs = [
         dejavu_fonts
-        noto-fonts-emoji
         fira-code
+        noto-fonts-emoji
     ];
 in
 stdenv.mkDerivation {
     pname = "nigelhaney.com";
-    version = "1.0.0";
+    version = "1";
     src = ./src;
 
     nativeBuildInputs = [
         python3
         chromium
-    ] ++ fontPkgsForChrome;
+    ] ++ fontPkgs;
 
-    # Chrome won't load any fonts by default, add a font config file so it does.
+    # Chrome won't load any fonts by default, add a font config file that loads the fonts we need when printing.
     FONTCONFIG_FILE = makeFontsConf {
-        fontDirectories = fontPkgsForChrome;
+        fontDirectories = fontPkgs;
     };
 
     buildPhase = ''
@@ -28,12 +28,19 @@ stdenv.mkDerivation {
         # Copy html files from src to output directory
         cp -rf . $out/
 
-        cp $FONTCONFIG_FILE $out/
+        # Copy fonts to resources in order to self-host fonts.
+        mkdir -p $out/resources/fonts
+
+        #cp ${fira-code}/share/fonts/truetype/FiraCode-VF.ttf $out/resources/fonts/fira-code.ttf
+
+        #cp ${dejavu_fonts}/share/fonts/truetype/DejaVuSans.ttf $out/resources/fonts/dejavu-sans.ttf
+        #cp ${dejavu_fonts}/share/fonts/truetype/DejaVuSans-Bold.ttf $out/resources/fonts/dejavu-sans-bold.ttf
+        #cp ${dejavu_fonts}/share/fonts/truetype/DejaVuSans-Oblique.ttf $out/resources/fonts/dejavu-sans-italic.ttf
+
+        ## TODO: See if we can reduce the size of this by only including emojis we need.
+        #cp ${noto-fonts-emoji}/share/fonts/noto/NotoColorEmoji.ttf $out/resources/fonts/noto-color-emoji.ttf
 
         # Use chrome to create PDF of resume.
-        python -m http.server -d $out &
-        serverpid=$!
         chromium-browser --no-sandbox --headless --no-margins --no-pdf-header-footer --print-to-pdf-no-header --run-all-compositor-stages-before-draw --print-to-pdf=$out/resources/resume.pdf $out/resume/index.html
-        kill $serverpid
     '';
 }
